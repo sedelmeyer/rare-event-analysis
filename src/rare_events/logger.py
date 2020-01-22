4992508dea2231c2ee840b8794db1218f6c78da8
sedelmeyer <20605812+sedelmeyer@users.noreply.github.com>
2020-01-22 07:17:08 -0500

    feat(log): revise logger wrap to include run time and additional details

--- src/rare_events/logger.py
+++ src/rare_events/logger.py
@@ -4,34 +4,44 @@ This module contains the logging functions used throughout the
 """
 
 import logging
+import time
 from functools import wraps
+
+log_file = '{}.log'.format(__name__)
+log_level = logging.INFO
+logging.basicConfig(
+    level=log_level, filename=log_file, filemode="w+",
+    format="%(asctime)-15s %(levelname)-8s %(message)s"
+    )
 
 
 def logger(orig_func):
-    logging.basicConfig(
-        filename='{}.log'.format(orig_func.__name__),
-        level=logging.INFO
-        )
 
     @wraps(orig_func)
     def wrapper(*args, **kwargs):
+        t1 = time.time()
+        logging.info('Run function {}'.format(orig_func.__name__))
         logging.info(
             'Ran with args: {}, and kwargs: {}'.format(args, kwargs)
             )
-        return orig_func(*args, **kwargs)
+        logging.info(orig_func.__doc__.partition('\n')[0])
+        result = orig_func(*args, **kwargs)
+        logging.info(
+            '{} ran in: {} sec'.format(orig_func.__name__, time.time() - t1)
+            )
+        return result
 
     return wrapper
 
 
 def timer(orig_func):
-    import time
 
     @wraps(orig_func)
     def wrapper(*args, **kwargs):
         t1 = time.time()
         result = orig_func(*args, **kwargs)
         t2 = time.time() - t1
-        print('{} ran in: {} sec'.format(orig_func.__name__, t2))
+        logging.info('{} ran in: {} sec'.format(orig_func.__name__, t2))
         return result
 
     return wrapper
